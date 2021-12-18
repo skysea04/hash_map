5fd6b6c922997889ea8fde04803febb3db9813b3
skysea <arcade0425@gmail.com>
2021-12-19 01:07:31 +0800

    fix djb2_hash run process

--- djb2_hash.py
+++ djb2_hash.py
@@ -1,8 +1,12 @@
+from basic_hash import do_hash_process
+
+
 def djb2_hash(key):
     hash = 5381
     for c in key:
         hash = hash * 33 + ord(c)
     return hash % 1013
+
 
 class HashTable:
     
@@ -19,7 +23,7 @@ class HashTable:
     def remove(self, key):
         self.table[djb2_hash(key)] = None
     
-    def show(self):
+    def display(self):
         count = 0
         for i in range(len(self.table)):
             if self.table[i]:
@@ -28,21 +32,4 @@ class HashTable:
         print("count:", count)
 
 
-hash_table = HashTable()
-hash_table.put('Skysea', 'arcade0000@gmail.com')
-hash_table.put('Lucas', 'lucas0101@gmail.com')
-hash_table.put('Ariana', 'ariana0202@gmail.com')
-hash_table.put('Andy', 'andy0303@gmail.com')
-hash_table.put('Tony', 'tony0404@gmail.com')
-hash_table.put('WeiChen', 'weichen0505@gmail.com')
-hash_table.put('Ton', 'wyt0606@gmail.com')
-hash_table.put('Sol', 'sol0707@gmail.com')
-hash_table.put('YCLee', 'yclee0808@gmail.com')
-hash_table.put('YuehHsin', 'yuehhsin0909@gmail.com')
-
-print(hash_table.get('Skysea'))
-print(hash_table.get('Lucas'))
-print(hash_table.get('WeiChen'))
-
-print("-" * 40)
-hash_table.show()
+do_hash_process(HashTable)
